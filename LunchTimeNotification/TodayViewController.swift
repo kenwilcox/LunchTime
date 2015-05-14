7f87adf7a06d57db3599b2f8b3bb71087dc617a4
Ken Wilcox <wilcoxk@gmail.com>
2015-05-13 23:15:17 -0600

    ergo, the UI isn't working

---
+++ LunchTimeNotification/TodayViewController.swift
@@ -0,0 +1,38 @@
+//
+//  TodayViewController.swift
+//  LunchTimeNotification
+//
+//  Created by Kenneth Wilcox on 5/13/15.
+//  Copyright (c) 2015 Kenneth Wilcox. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view from its nib.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  func widgetMarginInsetsForProposedMarginInsets(defaultMarginInsets: UIEdgeInsets) -> UIEdgeInsets {
+    return UIEdgeInsetsMake(8.0, 16.0, 8.0, 16.0)
+  }
+  
+  func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)!) {
+    // Perform any setup necessary in order to update the view.
+    
+    // If an error is encountered, use NCUpdateResult.Failed
+    // If there's no update required, use NCUpdateResult.NoData
+    // If there's an update, use NCUpdateResult.NewData
+    
+    completionHandler(NCUpdateResult.NewData)
+  }
+
+}
